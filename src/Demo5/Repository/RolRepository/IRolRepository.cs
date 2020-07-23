73223a696e97d3aeeae16ed707c760d54c157ada
David Jiménez <david.jimenez@altia.es>
2020-07-23 08:44:12 +0200

    * Added repository Rol

--- src/Demo5/Repository/RolRepository/IRolRepository.cs
+++ src/Demo5/Repository/RolRepository/IRolRepository.cs
@@ -1,10 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Database.Model;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace Repository.RolRepository
 {
     public interface IRolRepository
     {
+        IQueryable<Rol> Get(int id);
+
+        IQueryable<Rol> GetList();
+
+        Task<Rol> FindAsync(int id);
+
+        Task<Rol> AddAsync(Rol item);
+
+        Task<int?> UpdateAsync(Rol item);
+
+        Task<int?> Delete(Rol item);
     }
 }
--- src/Demo5/Repository/RolRepository/RolRepository.cs
+++ src/Demo5/Repository/RolRepository/RolRepository.cs
@@ -24,5 +24,40 @@ namespace Repository.RolRepository
 
         public async Task<Rol> FindAsync(int id)
             => await _context.Rol.FindAsync(id);
+
+        public async Task<Rol> AddAsync(Rol item)
+        {
+            if (item != null)
+            {
+                await _context.Rol.AddAsync(item);
+                await _context.SaveChangesAsync();
+
+                return item;
+            }
+
+            return null;
+        }
+
+        public async Task<int?> UpdateAsync(Rol item)
+        {
+            if (item != null)
+            {
+                _context.Rol.Update(item);
+                return await _context.SaveChangesAsync();
+            }
+
+            return null;
+        }
+
+        public async Task<int?> Delete(Rol item)
+        {
+            if (item != null)
+            {
+                _context.Rol.Remove(item);
+                return await _context.SaveChangesAsync();
+            }
+
+            return null;
+        }
     }
 }
